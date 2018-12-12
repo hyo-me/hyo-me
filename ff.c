0ea2e3fb19a1ea507357ffa1631b4b46f106b730
hyo-me <42805831+hyo-me@users.noreply.github.com>
2018-12-12 15:35:36 +0900

    Update ff.c

--- ff.c
+++ ff.c
@@ -4,7 +4,6 @@ make strtok  2018044984 hyo-jeong
 
 */
 
-
 #include <stdio.h>
 #include <string.h>
 char* hyo_strtok();
@@ -18,16 +17,16 @@ int main(void){
 	 char* fuk = strtok(str1,sep);
 	 while(fuk != NULL){
 	     printf("%s\n",fuk);
-	 	 fuk = strtok(NULL,sep);
-	 	 }
+	     fuk = strtok(NULL,sep);
+	     }
 	 
 	 printf("\n");
 
 	 char* fuc = hyo_strtok(str2,sep);
 	 while(fuc != NULL){
 	     printf("%s\n",fuc);
-		 fuc = hyo_strtok(NULL,sep);
-		 }
+	     fuc = hyo_strtok(NULL,sep);
+	     }
 
 	 return 0;
 }
@@ -44,10 +43,10 @@ char* hyo_strtok(char *str,const char *sep){
 	 int l2 = strlen(sep);
 	 int i,k;
 
-     for(i = 0;i<l1;i++){
+         for(i = 0;i<l1;i++){
 	     for(k = 0;k<l2;k++){
 		     if(fuc[i] == sep[k]) break;
-			 }
+		     }
 		 if(k == l2) break;
 		 }
 
@@ -56,14 +55,14 @@ char* hyo_strtok(char *str,const char *sep){
 	 for(i = 0;i<l1;i++){
 	     for(k = 0;k<l2;k++){
 		     if(fuc[i] == sep[k]) break;
-			 }
+		     }
 		 if(k<l2){
 		     strcpy(ff,fuc);
-			 ff[i] = 0;
-			 fuc += i+1;
+		     ff[i] = 0;
+		     fuc += i+1;
 			 
-			 return ff;
-			 }
+		     return ff;
+	             }
 		 }
 	
 	 return NULL;
